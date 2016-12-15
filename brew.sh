187b945e4fa24f515ac42981277190077332e321
Jean-Baptiste Cordonnier <jb.cdnr@gmail.com>
2016-12-15 16:38:18 +0100

    Update deprecated brew formula

--- brew.sh
+++ brew.sh
@@ -23,7 +23,7 @@ brew install gnu-sed --with-default-names
 brew install wget --with-iri
 
 # Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
+brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
