a00b7219603ce2d89de8252895171c56b3256852
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2016-12-16 09:49:34 +0100

    Add mac specific settings

---
+++ macos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Speed up the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0.5
