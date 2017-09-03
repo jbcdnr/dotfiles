88149ea4630d7f44ad76cd2fcd021e77b0e50288
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2017-09-03 15:24:30 +0200

    Add hot corners for macOS

--- macos.sh
+++ macos.sh
@@ -8,3 +8,13 @@ defaults write com.apple.dock autohide-delay -float 0
 
 # Speed up the animation when hiding/showing the Dock
 defaults write com.apple.dock autohide-time-modifier -float 0.5
+
+# Hot corners
+defaults write com.apple.dock wvous-bl-corner -integer 4        # Bottom left Desktop
+defaults write com.apple.dock wvous-bl-modifier -integer 131072 # with Shift
+defaults write com.apple.dock wvous-tl-corner -integer 1        # Top left nothing
+defaults write com.apple.dock wvous-tr-corner -integer 1        # Top right nothing
+defaults write com.apple.dock wvous-br-corner -integer 1        # Bottom right nothing
+
+# Reload
+killall Dock
