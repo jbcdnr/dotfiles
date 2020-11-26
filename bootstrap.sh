f15ef74bb30f272ab265d2e158afb40ae0304efb
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2020-11-26 10:38:13 +0000

    Link .vim directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git status
 
-declare -a dotfiles=('zshrc' 'zshenv' 'bashrc' 'gitconfig' 'gitignore_global' 'vimrc' 'paths' 'extra' 'exports' 'aliases' 'prompt' 'tmux.conf')
-
-declare -a dirs=('.vim .ssh')
+declare -a dotfiles=('zshrc' 'zshenv' 'bashrc' 'gitconfig' 'gitignore_global' 'vimrc' 'paths' 'extra' 'exports' 'aliases' 'prompt' 'tmux.conf' 'vim' 'ssh')
 
 function doIt() {
   for dotfile in "${dotfiles[@]}"; do
@@ -15,9 +13,6 @@ function doIt() {
       ln -s "`pwd`/.$dotfile" "$HOME/.$dotfile"
     fi
   done
-  rsync  --exclude ".gitignore" \
-    --exclude ".DS_Store" \
-    -avh --no-perms "${dirs[@]}" ~;
 
   # link sublime text settings for macOS
   [[ `uname` == "Darwin" ]] && ln -fs "$HOME/dotfiles/ressources/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
@@ -38,4 +33,3 @@ else
 fi;
 unset doIt;
 unset dotfiles;
-unset dirs;
